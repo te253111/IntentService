ef5f972aeda6ad50028dcd94afd7333c493d14de
kanok <kanok@csc.co.th>
2016-07-27 10:03:29 +0700

    New changelist

--- app/src/main/java/com/example/cylonz/intentservice/MyTestService.java
+++ app/src/main/java/com/example/cylonz/intentservice/MyTestService.java
@@ -2,23 +2,26 @@ package com.example.cylonz.intentservice;
 
 import android.app.IntentService;
 import android.content.Intent;
+import android.os.Handler;
 import android.util.Log;
+import android.widget.Toast;
 
 /**
  * Created by Cylonz on 25/7/2559.
  */
 
 public class MyTestService extends IntentService {
-
+    Handler mHandler;
     public MyTestService() {
         super("testservice");
+        mHandler = new Handler();
     }
     @Override
     protected void onHandleIntent(Intent intent) {
        for(int i=0;i<100;i++) {
            try {
                Thread.sleep(1000);
-               Log.d("test", String.valueOf(i));
+               mHandler.post(new DisplayToast(this, "Num"+String.valueOf(i)));
            } catch (InterruptedException e) {
                e.printStackTrace();
            }
